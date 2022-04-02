6f0a33874b1a9b68c9b045b22e6f09db46354f4d
chenpeiu <102472841+chenpeiu@users.noreply.github.com>
2022-04-02 18:24:20 +0800

    start

---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+  .maskmap
+    .searchbar.w-96.h-screen.bg-slate-300
+      select(v-model="city")
+        option.text-center(disabled value="") 請選擇地區
+        option.text-center A
+
+</template>
+<style lang="sass">
+  .maskmap
+    .searchbar
+      .item
+        width: 80%
+</style>
+<script>
+import axios from 'axios'
+export default {
+  async fetch(){
+    await axios.get("https://raw.githubusercontent.com/donma/TaiwanAddressCityAreaRoadChineseEnglishJSON/master/AllData.json")
+                .then( response => this.mapapi = response)
+    // await axios.get("https://raw.githubusercontent.com/donma/TaiwanAddressCityAreaRoadChineseEnglishJSON/master/AllData.json")
+    //             .then( response => this.cityapi = response)
+    console.log(this.mapapi)
+  },
+  data(){
+    return{
+      mapapi: [],
+      cityapi: [],
+    }
+  }
+}
+</script>
